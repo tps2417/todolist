d478dbfb5ae2ef6e8888ac63a3cbe87e550d17a7
tps2417 <fmfmkkk@yahoo.co.jp>
2022-11-17 14:57:05 +0900

    入力した値をstateに更新する

--- src/screens/Todo.js
+++ src/screens/Todo.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "../components/Card";
 
 export const Todo = () => {
+  // const [state, setState] = useState("");
+  const [text, setText] = useState("");
+
+  const inputText = (event) => {
+    setText(event.target.value);
+  };
+
   return (
     <div style={styles.todoContainer}>
-      <div>Todo</div>
+      <div>
+        <input id="text" onChange={(e) => inputText(e)}></input>
+        <button>＋</button>
+      </div>
+
       <div style={styles.card}>
         <Card />
       </div>
